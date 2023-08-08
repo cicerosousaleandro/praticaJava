6cf84813435a31b9a42cab7c6778a1b118e378e4
Cícero Sousa <cicerosousa.le@gmail.com>
2023-08-07 22:45:39 -0300

    Finalizando o projeto

--- Cadastro de produto/couse/src/application/program.java
+++ Cadastro de produto/couse/src/application/program.java
@@ -22,8 +22,25 @@ public class program {
 		System.out.print("Quantity in stoke: ");
 		product.quantity = sc.nextInt();
 		
+		System.out.println();
+		System.out.println("product data:" + product);
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock:" );
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data:" + product);
+		
+
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock:" );
+		quantity = sc.nextInt();
+	    product.removeProducts(quantity);
+	    
+	    System.out.println();
+		System.out.println("Update data:" + product);
 		
 		sc.close();
 	}
