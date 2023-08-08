a616173f12244ea83df596e6b392b60ca72170e2
Cícero Sousa <cicerosousa.le@gmail.com>
2023-08-07 22:20:37 -0300

    aplicando toString

--- Cadastro de produto/couse/src/entities/Product.java
+++ Cadastro de produto/couse/src/entities/Product.java
@@ -20,5 +20,17 @@ public class Product {
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
+	
+	public String toString() {
+		return name
+		+ " , $ "
+		+ String.format("%.2f", price)
+		+ ", "
+		+ quantity
+		+ " units, Total; $ "
+		+ String.format("%.2f", totalValueInStock());
+		
+				
+	}
 }
 
